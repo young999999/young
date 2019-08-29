6e59f5e56ddf2e3555448dc25893016ffdff3d91
young999999 <1972780113@qq.com>
2019-08-29 16:55:20 +0800

    完善eureka，建立集群

--- provider-dept-8001/src/main/java/com/py/springcloud/controller/DeptController.java
+++ provider-dept-8001/src/main/java/com/py/springcloud/controller/DeptController.java
@@ -2,9 +2,12 @@ package com.py.springcloud.controller;
 
 import java.util.List;
 
-import com.py.springcloud.service.impl.DeptServiceImpl;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +23,7 @@ public class DeptController
 	@Autowired
 	private DeptService service;
 
-//	@Autowired
-//	private DiscoveryClient client;
+
 
 	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
 	public boolean add(@RequestBody Dept dept)
@@ -42,20 +44,20 @@ public class DeptController
 	}
 
 	
-//	@Autowired
-//	private DiscoveryClient client;
-//	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
-//	public Object discovery()
-//	{
-//		List<String> list = client.getServices();
-//		System.out.println("**********" + list);
-//
-//		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
-//		for (ServiceInstance element : srvList) {
-//			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
-//					+ element.getUri());
-//		}
-//		return this.client;
-//	}
+	@Autowired
+	private DiscoveryClient client;
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
 
 }
