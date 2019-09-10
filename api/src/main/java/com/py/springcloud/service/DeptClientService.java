5e0e8c81832622f5e143f4cd335fc0156bc2fccd
young999999 <1972780113@qq.com>
2019-09-10 19:38:58 +0800

    加入服务熔断hystrix的应用模块

--- api/src/main/java/com/py/springcloud/service/DeptClientService.java
+++ api/src/main/java/com/py/springcloud/service/DeptClientService.java
@@ -21,8 +21,8 @@ import java.util.List;
  * @author zzyy
  * @date 2018年4月21日
  */
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
-//@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
 public interface DeptClientService
 {
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
